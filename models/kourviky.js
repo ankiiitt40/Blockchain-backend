0beb6230ab200f98161a3742c7f47f6b95718d3f
KourvikyShrivas <kourvikyshrivas06010@gmail.com>
2025-12-18 11:48:45 +0530

    Added kourviky model

---
+++ models/kourviky.js
@@ -0,0 +1,12 @@
+// Arithmetic
+let sum = 5 + 3;
+let diff = 5 - 3;
+
+// Comparison
+5 == "5";   // true (value only)
+5 === "5";  // false (value + type)
+
+// Logical
+true && false; // AND
+true || false; // OR
+!true;         // NOT
